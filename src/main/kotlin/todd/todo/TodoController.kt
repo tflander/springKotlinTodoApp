61c75e941bfbeeb1ca61785ff305fd65dee49ae1
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-06-12 16:27:47 -0400

    support all endpoints from chapter one of book

--- src/main/kotlin/todd/todo/TodoController.kt
+++ src/main/kotlin/todd/todo/TodoController.kt
@@ -1,10 +1,9 @@
 package todd.todo
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +17,23 @@ class TodoController {
         return todoService.todosForName(name)
     }
 
+
+    @GetMapping("users/{name}/todo/{id}")
+    fun retrieveById(@PathVariable name: String, @PathVariable id: Int): Any {
+        val todoOptional = todoService.byId(id)
+        return if (todoOptional.isPresent) {
+            todoOptional.get()
+        } else {
+            "Todo with id $id not found"
+        }
+    }
+
+    @PostMapping("/users/{name}/todo")
+    fun addTodoForUser(@PathVariable name: String, @RequestBody todo: Todo): ResponseEntity<*> {
+        val (id) = todoService.add(todo.user!!, todo.desc!!, todo.targetDate!!)
+                ?: return ResponseEntity.noContent().build<Any>()
+        val uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri()
+        return ResponseEntity.created(uri).build<Any>()
+    }
 }
 
