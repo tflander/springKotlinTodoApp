c906ce6f6678ad9ccf5627abf63d87f4ffcae8bd
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-06-15 00:03:25 -0400

    first pass at implementing HATEOAS

--- src/main/kotlin/todd/todo/TodoController.kt
+++ src/main/kotlin/todd/todo/TodoController.kt
@@ -1,6 +1,9 @@
 package todd.todo
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.hateoas.Resource
+import org.springframework.hateoas.mvc.ControllerLinkBuilder
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
@@ -21,11 +24,15 @@ class TodoController {
     @GetMapping("users/{name}/todo/{id}")
     fun retrieveById(@PathVariable name: String, @PathVariable id: Int): Any {
         val todoOptional = todoService.byId(id)
-        return if (todoOptional.isPresent) {
-            todoOptional.get()
-        } else {
+
+        if (!todoOptional.isPresent) {
             throw TodoNotFoundException("Todo with id $id not found")
         }
+
+        val resource = Resource<Todo>(todoOptional.get())
+        val linkbyId: ControllerLinkBuilder = linkTo(methodOn(this::class.java).retrieveForName(name))
+        resource.add(linkbyId.withRel("parent"))
+        return resource
     }
 
     @PostMapping("/users/{name}/todo")
@@ -33,6 +40,7 @@ class TodoController {
         val (id) = todoService.add(todo.user, todo.desc, todo.targetDate)
                 ?: return ResponseEntity.noContent().build<Any>()
         val uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri()
+
         return ResponseEntity.created(uri).build<Any>()
     }
 }
--- src/test/kotlin/todd/todo/TodoControllerTest.kt
+++ src/test/kotlin/todd/todo/TodoControllerTest.kt
@@ -4,6 +4,7 @@ import assertk.assert
 import assertk.assertions.isEqualTo
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.`when`
@@ -12,14 +13,20 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
+import todd.utilities.JsonFileReader
+import java.io.IOException
 import java.time.LocalDate
 import java.util.*
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoControllerTest {
+
+    @LocalServerPort
+    var port: Int = 0
 
     @Autowired
     lateinit var testRestTemplate: TestRestTemplate
@@ -29,6 +36,15 @@ class TodoControllerTest {
 
     @MockBean
     lateinit var todoService: TodoService
+
+//    @Test
+//    fun retrieveTodosForUser() {
+//        val response = testRestTemplate.getForEntity(createURL("/api/users/Todd/todos"), String::class.java)
+//
+//        val actual = ObjectMapper().readTree(response.getBody())
+//        val expected = getExpectedJsonForRetrieveTodos()
+//        assert(actual).isEqualTo(expected)
+//    }
 
     @Test
     fun retrieveForName() {
@@ -60,16 +76,23 @@ class TodoControllerTest {
     @Test
     fun retrieveByIdHappyPath() {
 
-        val todo = Todo(0, "todd", "descr", LocalDate.now(), false)
+        val todo = Todo(0, "Todd", "descr", LocalDate.of(2018, 6,8), false)
         `when`(todoService.byId(0)).thenReturn(Optional.of(todo))
 
         val result = testRestTemplate.getForEntity("/api/users/Todd/todo/0",
                 String::class.java)
         assert(result.statusCode).isEqualTo(HttpStatus.OK)
+        val expected = objectMapper
+                .readTree(JsonFileReader()
+                .readJsonFromClasspath("expectedResponses/retrieveTodoById.json"))
+                .toString()
+                .replace("8080", port.toString())
+
+        print(expected.toString())
 
         JSONAssert.assertEquals(
-                objectMapper.writeValueAsString(todo),
-                result.body,
+                expected,
+                objectMapper.readTree(result.body).toString(),
                 true
         )
     }
@@ -99,6 +122,18 @@ class TodoControllerTest {
         val result = testRestTemplate.postForEntity("/api/users/Todd/todo", jsonNode, JsonNode::class.java)
         assert(result.statusCode).isEqualTo(HttpStatus.CREATED)
         assert(result.headers.get("location")).equals("/users/Todd/todo/4")
+    }
+
+    @Throws(IOException::class)
+    private fun getExpectedJsonForRetrieveTodos(): JsonNode {
+        val expected = objectMapper.readTree(JsonFileReader().readJsonFromClasspath("expectedResponses/retrieveTodosForUser.json"))
+        val today = LocalDate.now()
+        expected.forEach { node -> (node as ObjectNode).replace("targetDate", objectMapper.valueToTree(today)) }
+        return expected
+    }
+
+    private fun createURL(path: String): String {
+        return "http://localhost:" + port + path
     }
 
 }
